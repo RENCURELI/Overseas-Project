824fc680b86a898ff5103126668eac4f093e5f6f
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-03-13 23:09:25 +0100

    Ajout des rotations de la caméra.

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -6,7 +6,9 @@ public class Player_Controller : MonoBehaviour
     [SerializeField]
     private float fVitesse = 1f; //Note fVitesse pour une variable vitesse de type float
     [SerializeField]
-    private float fRegard = 3f; //Pour la sensibilité de la souris pour bouger la caméra.
+    private float fSensibiliteRegard = 3f; //Pour la sensibilité de la souris tourner le joueur.
+    [SerializeField]
+    private float fSensibiliteCamera = 2f; //Pour la sensibilité de la souris pour orienter la camera.
 
     private Player_Movements sMouvement; //sMouvement pour une variable mouvement de type script
 
@@ -34,10 +36,19 @@ public class Player_Controller : MonoBehaviour
         sMouvement.Mouvement(vVelocite);
 
         //Gestion des rotations de la caméra.
+        //Rotation selon Y pour tout le joueur.
         float fRotationY = Input.GetAxisRaw("Mouse X"); //Récupération des inputs d'axes par défaut avec l'axe horizontal de la souris.
 
-        Vector3 vRotation = new Vector3(0, fRotationY, 0)*fRegard;
+        Vector3 vRotation = new Vector3(0, fRotationY, 0)* fSensibiliteRegard;
 
         sMouvement.Rotation(vRotation);
+
+
+        //Rotation selon X pour la caméra.
+        float fRotationX = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 vRotationCamera = new Vector3(fRotationX, 0, 0) * fSensibiliteCamera;
+
+        sMouvement.RotationCamera(vRotationCamera);
     }
 }
